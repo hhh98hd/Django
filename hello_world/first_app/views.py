272c7ec9b13afba20bb9c77497209206c9c9afb3
hoanghh <hhh10anhchuyennt@gmail.com>
2022-11-29 22:48:00 +0700

    static

--- hello_world/first_app/views.py
+++ hello_world/first_app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 def index(req):
     dict = {
-        'index_header' : 'This is H1 header'
+        'index_header' : 'FPT Software'
     }
     return render(req, 'first_app/index.html', context=dict)
